36b50331c14b79a281b577b8f407c377a4598bae
AITuring <shspku365@gmail.com>
2020-04-14 20:47:27 +0800

    anime

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const glide = new Glide(".glide");
+const captionsEL = document.querySelector(".slide-caption");
+
+gilde.on(["mount.after","run.after"],() => {
+    const caption = captionsEL[gilde.index];
+    anime({
+        targets: caption.children,
+        opacity:[0 ,1],
+        duration:400,
+        easing:"linear",
+        // 在300ms后出第一项，然后每隔400ms出现其他
+        delay:anime.stagger(400,{start:300}),
+        // 每一项在y轴移动距离，第一个是40到0，第二个是25,0，第三个是10,0
+        translateY:[anime.stagger(40,10),0]
+    });
+})
 glide.mount();
