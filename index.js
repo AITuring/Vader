22cc38be0f8405d453d94fa22f6e7c3ff09a8ddb
AITuring <shspku365@gmail.com>
2020-04-15 06:01:24 +0800

    基本完成

--- index.js
+++ index.js
@@ -21,3 +21,22 @@ glide.on("run.before", () => {
     })
 })
 glide.mount();
+
+
+const isotope = new Isotope(".cases",{
+    layoutMode:"fitRows",
+    itemSelector:".case-item"
+});
+
+const filterBtns = document.querySelector(".filter-btns");
+
+filterBtns.addEventListener("click",e => {
+    let {target} = e;
+    const filterOption = target.getAttribute("data-filter");
+    if (filterOption) {
+        document.querySelectorAll(".filter-btn.active").forEach(btn => btn.classList.remove("active"));
+        target.classList.add("active");
+
+        isotope.arrange({filter:filterOption});
+    }
+})
